ed795f180d7612f481b717d0d6299e7c9b7ee69b
Michael Whitley <michaelwhitley@gmail.com>
2015-12-23 08:33:41 -0600

    day 17 complete solution

--- 17/code.js
+++ 17/code.js
@@ -1,95 +1,112 @@
 var fs = require('fs');
 var filename = 'input.txt';
 
-var amountToStore = 25;
+var amountToStore = 150;
 
-var permArr = [];
-var usedChars = [];
-var rows = [];
+var combos = [];
+var containers = [];
 
-function permute(input) {
-  var i, ch;
-  for (i = 0; i < input.length; i++) {
-    ch = input.splice(i, 1)[0];
-    usedChars.push(ch);
-    if (input.length == 0) {
-      permArr.push(usedChars.slice());
-    }
-    permute(input);
-    input.splice(i, 0, ch);
-    usedChars.pop();
-  }
-  return permArr
-};
-
-function matches(combos, newcombo) {
-	for(var combo of combos) {
-		if(combo.toString() === newcombo.toString()) {
-			return true;
-		}
-	}
-
-	return false;
-}
-
-function comboTotal(combo) {
-	var total = 0;
-	for (var number of combo) {
-		total += Number(rows[number]);
-	}
-
-	return total;
-}
-
-function findCombos(containers) {
-	var combos = [];
-
-	for(var container of containers) {
-		var amountLeft = amountToStore;
-		var innerCombo = [];
-
-		while(amountLeft > 0) {
-			if(container.length === 0) {
-				amountLeft = 0;
-			}
-			else if(container[0].value <= amountLeft) {
-				amountLeft -= container[0].value;
-				innerCombo.push(Number(container[0].index));
-				container.shift();
-			}
-			else{
-				container.shift();
-			}
-			
-		}
-		innerCombo = innerCombo.sort(function (a, b) { 
-		    return a - b;
-		});
-
-		if(!matches(combos, innerCombo)) {
-			if(comboTotal(innerCombo) === amountToStore) {
-				combos.push(innerCombo);
-			}
-		}
-
-		
-	}
-	return combos;
-}
+var winners = [];
+var losers = [];
 
 var time = {start: new Date()};
 
+function comboCapacity(c) {
+	var total = 0;
+	for (var i = 0; i < c.length; i++) {
+		total += c[i].value;
+	}
+	return total;
+}
+
+function findCombos() {
+
+
+	for(var i = 0; i < containers.length; i++) {
+		var c = containers[i];
+		var r = containers.slice(0);
+		r.splice(i,1);
+		combos.push({
+			combo: [containers[i]], 
+			remaining: r
+		});
+	}
+
+	while(combos.length > 0) {
+		console.log(combos.length);
+		for(var i = 0; i < combos.length; i++) {
+			for(var j = 0; j < combos[i].remaining.length; j++) {
+
+				var clonedCombo = JSON.parse(JSON.stringify(combos[i]));
+				clonedCombo.combo.push(combos[i].remaining[j]);
+				clonedCombo.remaining.splice(j, 1);
+
+				if(comboCapacity(clonedCombo.combo) === amountToStore) {
+					addCombo(clonedCombo);
+					continue;
+				}
+				else if(comboCapacity(clonedCombo.combo) < amountToStore) {
+					combos.push(clonedCombo);
+				}
+				else if(comboCapacity(clonedCombo.combo) > amountToStore) {
+					//losers.push(clonedCombo);
+					continue;
+				}
+			}
+
+			combos.splice(i, 1);
+		}
+	}
+
+
+}
+
+function addCombo(c) {
+	var shouldPush = true;
+	c.combo.sort(function(a,b) {
+		return a.index - b.index;
+	});
+
+	var cString = JSON.stringify(c.combo);
+
+	for(var w of winners) {
+		var wString = JSON.stringify(w.combo);
+		if (wString == cString) {
+			shouldPush = false;
+		}
+	}
+
+	if(shouldPush) {
+		winners.push(c);
+	}
+	
+}
+
+
+function finish() {
+	time.stop = new Date();
+	console.log("Part One | Combinations to hold eggnog: " + winners.length);
+	console.log((time.stop - time.start) / 1000 + " seconds");
+
+	var minimum = winners[0].combo.length;
+
+	var mimimumCount = 0;
+	for(var i = 0; i < winners.length; i++) {
+		var w = winners[i];
+		if(w.combo.length === minimum) {
+			mimimumCount++;
+		}
+	}
+
+	console.log("Part Two | Minimum to hold: " + mimimumCount);
+}
+
 fs.readFile(filename, 'utf8', function(err, data) {
 	rows = data.split('\n');
-	var containers = [];
 	for(var i in rows) {
-		containers.push({index: i, value: rows[i]});
+		containers.push({index: i, value: parseInt(rows[i])});
 	}
-	var combos = permute(containers);
-	console.log(combos.length);
-	var combinationsToHoldEggnog = findCombos(combos);
-	time.stop = new Date();
-	console.log("Part One | Combinations to hold eggnog: " + combinationsToHoldEggnog.length);
-	console.log((time.stop - time.start) / 1000 + " seconds");
+	findCombos();
+	finish();
 
 });
